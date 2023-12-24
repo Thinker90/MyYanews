ba6ac839650f24f92abd358bfa7e02a56a539e1e
Thinker90 <stas.basov@whatever.com>
2023-12-25 00:12:22 +0500

    2/6 complete Yanews test_routes.py / test_content.py

---
+++ news/pytest_tests/test_content.py
@@ -0,0 +1,34 @@
+import pytest
+from http import HTTPStatus
+from django.urls import reverse
+from pytest_django.asserts import assertRedirects
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name',
+    ('news:home',)
+)
+def test_home_page(client, news, name):
+    for i in range(11):
+        news
+    url = reverse(name)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.context['object_list']) <= 10
+    news_list = response.context['object_list']
+    dates = [news.date for news in news_list]
+    assert dates == sorted(dates, reverse=True)
+
+
+@pytest.mark.django_db
+def test_home_page(client, id_news, comment, news):
+    for i in range(2):
+        comment
+    url = reverse('news:detail', args=id_news)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.OK
+    news = response.context['news']
+    dates = [comment.created for comment in news.comment_set.all()]
+    assert dates == sorted(dates, reverse=True)
+    assert 'form' not in response.context
